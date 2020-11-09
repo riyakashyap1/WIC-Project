c2eebd2b05eeeb70115dcbba10c78abee77dae01
Lacey Umamoto <lcumamoto@gmail.com>
2020-11-09 15:49:32 -0800

    added starter code

---
+++ ios/Pods/Target Support Files/Pods-BR4-tvOS/Pods-BR4-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BR4_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_BR4_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-BR4/Pods-BR4-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BR4 : NSObject
+@end
+@implementation PodsDummy_Pods_BR4
+@end
